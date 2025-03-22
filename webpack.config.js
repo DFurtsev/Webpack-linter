bc31f71d8191dcbfd0f6ababf69867393b0b35e3
Дмитрий <dimafurtcev387@gmail.com>
2025-03-22 13:53:32 +0300

    1

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const { merge } = require('webpack-merge');
+const path = require('path');
+const commonConfig = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    }
+}
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        return merge(commonConfig, {
+            mode: 'development',
+            devServer: {
+                static: './dist',
+                hot: true,
+            },
+        });
+    } else {
+        return merge(commonConfig, {
+            mode: 'production',
+        });
+    }
+}
